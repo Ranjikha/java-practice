e2c3a767a043a11db861570743ca441523d635cb
Ranjikha <ranjikharaja@gmail.com>
2026-07-27 23:18:09 +0530

    added new practice programs

---
+++ sum.java
@@ -0,0 +1,10 @@
+class sum{
+    public static void main(String args[]){
+        int[] arr={5,7,2,5,1};
+        int sum=0;
+        for(int i=0;i<arr.length;i++){
+            sum=sum+arr[i];
+        }
+        System.out.println(sum);
+    }
+}

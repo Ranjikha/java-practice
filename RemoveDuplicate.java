db121484d7a5c4b516441e8065149f2e5172babe
Ranjikha <ranjikharaja@gmail.com>
2026-07-30 23:14:23 +0530

    added new practice programs

---
+++ RemoveDuplicate.java
@@ -0,0 +1,17 @@
+class RemoveDuplicate{
+    public static void main(String args[]){
+        int[] arr={10,30,50,10,20,30};
+        for(int i=0;i<arr.length;i++){
+            boolean isDuplicate=false;
+            for(int j=0;j<i;j++){
+                if(arr[i]==arr[j]){
+                    isDuplicate=true;
+                    break;
+                }
+            }
+            if(!isDuplicate){
+                System.out.print(arr[i]+" ");
+            }
+        }
+    }
+}

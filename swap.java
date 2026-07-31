dfde42bb0d5be911daa8e538c64b5f9c97e3fa49
Ranjikha <ranjikharaja@gmail.com>
2026-07-31 22:22:36 +0530

    added new practice programs

---
+++ swap.java
@@ -0,0 +1,15 @@
+class swap {
+    public static void main(String args[]){
+        int a=10;
+        int b=20;
+        int temp;
+        System.out.println(a);
+        System.out.println(b);
+        temp=a;
+        a=b;
+        b=temp;
+        System.out.println(a);
+        System.out.println(b);
+    }
+    
+}

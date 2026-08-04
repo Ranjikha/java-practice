7284c9fe6f5e451814d4e2e7ff7d79df97df23e9
Ranjikha <ranjikharaja@gmail.com>
2026-08-04 18:44:22 +0530

    new added practice programs

---
+++ SimplePattern.java
@@ -0,0 +1,25 @@
+public class SimplePattern {
+    public static void main(String args[]){
+        int n=3;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(j);
+                if(j<i){
+                System.out.print(" *");
+                }
+            }
+            System.out.println();
+        }
+        for(int i=n;i>=1;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print(j);
+                if(j<i){
+                    System.out.print( " *");
+                }
+            }
+                System.out.println();
+            
+        
+    }
+}
+}

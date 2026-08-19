82dfb86a4be15be7a8a36dff3907631920749336
Ranjikha <ranjikharaja@gmail.com>
2026-08-19 21:29:23 +0530

    Create RecursionExample.java

---
+++ RecursionExample.java
@@ -0,0 +1,13 @@
+public class RecursionExample {
+    static void printNumbers(int n) {
+        if (n == 0) {   // Base case
+            return;
+        }
+        printNumbers(n - 1);   // Recursive call
+        System.out.println(n);
+    }
+
+    public static void main(String[] args) {
+        printNumbers(5);
+    }
+}

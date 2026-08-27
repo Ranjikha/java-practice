a8ef46be740d1b4fd6bb47c3d03941cc137d16e7
Ranjikha <ranjikharaja@gmail.com>
2026-08-27 22:08:46 +0530

    Create Vowels constants.java

---
+++ Vowels constants.java
@@ -0,0 +1,23 @@
+public class VowelsConsonants {
+    public static void main(String[] args) {
+
+        String str = "education";
+        int vowels = 0;
+        int consonants = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+
+            char ch = str.charAt(i);
+
+            if (ch == 'a' || ch == 'e' || ch == 'i' ||
+                ch == 'o' || ch == 'u') {
+                vowels++;
+            } else {
+                consonants++;
+            }
+        }
+
+        System.out.println("Vowels = " + vowels);
+        System.out.println("Consonants = " + consonants);
+    }
+}

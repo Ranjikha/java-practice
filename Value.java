6d424010228997c9409f55a7c3abd38852022621
Ranjikha <ranjikharaja@gmail.com>
2026-08-29 22:10:57 +0530

    Create Value.java

---
+++ Value.java
@@ -0,0 +1,10 @@
+public class Value{
+    public static void main(String[] args) {
+
+        String str = "abc";
+
+        for (int i = 0; i < str.length(); i++) {
+            System.out.println(str.charAt(i) + " = " + (int)str.charAt(i));
+        }
+    }
+}
